76279beef159ba70928ea8ecf38bfc8edf60df16
PJHRobles <joshuahrobles@gmail.com>
2017-04-01 04:23:01 +0800

    Added requirements.txt and controllers folder

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,6 +1,10 @@
 'use strict';
 
+import predictor from '../controllers/predictor';
+
 module.exports = (router) => {
+
+    router.get('/prediction', predictor.send_prediction);
 
     return router;
 }
---
+++ backend/controllers/predictor.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.send_prediction = (req, res, next) => {
+    res.send({});
+}
