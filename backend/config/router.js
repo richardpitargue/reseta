bad20627752784d38e5889bbb1bfd829eece7f27
PJHRobles <joshuahrobles@gmail.com>
2017-04-01 12:44:33 +0800

    for promise

--- backend/config/router.js
+++ backend/config/router.js
@@ -6,6 +6,7 @@ module.exports = (router) => {
 
     router.get('/prediction/sales', predictor.send_prediction_sales);
     router.get('/prediction/diseases', predictor.send_prediction_diseases);
+    router.get('/prediction/region', predictor.send_prediction_all_sales);
 
     return router;
 }
--- backend/controllers/predictor.js
+++ backend/controllers/predictor.js
@@ -49,6 +49,95 @@ exports.send_prediction_sales = (req, res, next) => {
     predict();
 }
 
+exports.send_prediction_all_sales = (req, res, next) => {
+
+    const data = {
+        month: req.query.month || 1,
+        year: req.query.year || 2016,
+        region: req.query.region || 'Region I',
+        medicine: req.query.medicine || 'ALBENDAZOLE 400MG TABLET',
+        disease: req.query.disease || 'Malaria',
+        cases: req.query.cases || 0,
+        population: req.query.population || 0,
+        density: req.query.density || 0
+    }
+
+    const MEDICINE = [
+                        'PARACETAMOL 325MG TABLET',
+                        'PARACETAMOL 500MG TABLET',
+                        'ALBENDAZOLE 400MG TABLET',
+                        'MEBENDAZOLE 100MG TABLET/CAPSULE',
+                        'DIPHENHYDRAMINE 25MG TABLET/CAPSULE',
+                        'CHLORPHENAMINE 4MG TABLET',
+                        'MEFENAMIC ACID 250MG TABLET/CAPSULE',
+                        'IBUPROFEN 200MG TABLET',
+                        'ASPIRIN 300MG TABLET',
+                        'ASPIRIN 325MG TABLET',
+                        'ASPIRIN 80MG TABLET',
+                        'LAGUNDI 300MG TABLET',
+                        'LAGUNDI 600MG TABLET',
+                        'SAMBONG 250MG TABLET',
+                        'SAMBONG 500MG TABLET',
+                        'DEXTROMETHORPHAN 10MG TABLET',
+                        'LOPERAMIDE 2MG CAPSULE',
+                        'MECLOZINE (MECLIZINE) (as hydrochloride) 12.5MG TABLET',
+                        'MECLOZINE (MECLIZINE) (as hydrochloride) 25MG TABLET',
+                        'BISACODYL 5MG TABLET',
+                        'STANDARD SENNA (Concentrate) 187MG TABLET',
+                        'FERROUS SULFATE 60MG TABLET',
+                        'ASCORBIC ACID (Vitamin C) 100MG TABLET',
+                        'ASCORBIC ACID (Vitamin C) 250MG TABLET',
+                        'ASCORBIC ACID (Vitamin C) 500MG TABLET',
+                        'VITAMIN B1 + B6 + B12 155MG TABLET/CAPSULE',
+                        'AMOXICILLIN (as trihydrate) 250MG CAPSULE',
+                        'AMOXICILLIN (as trihydrate) 500MG CAPSULE',
+                        'COTRIMOXAZOLE (sulfamethoxazole + trimethoprim) 480MG TABLET/CAPSULE',
+                        'COTRIMOXAZOLE (sulfamethoxazole + trimethoprim) 960MG TABLET/CAPSULE',
+                        'METFORMIN (as hydrochloride) 500MG TABLET',
+                        'GLIBENCLAMIDE 5MG TABLET',
+                        'METOPROLOL (as tartrate) 50MG TABLET',
+                        'CAPTOPRIL 25MG TABLET',
+                        'SALBUTAMOL (as sulfate) 2MG TABLET'
+    ]
+
+    function predict(med) {
+        let dirname = __dirname + '/../../scikit_regression.py';
+        let arr = [dirname, data.month, data.year, data.region, med, 
+                   data.disease, data.cases, data.population, data.density, 1]
+        let python = require("child_process").spawn('python', arr);
+
+        let output = '';
+        let response;
+        let type = '';
+
+        python.stdout.on('data', (_data) => {
+            output += _data.toString();
+
+            console.log("a" + output);
+            response = {
+                region: data.region,
+                medicine: med,
+                expected_demand: parseInt(output)
+            }
+
+            return response;
+        });
+    }
+    
+    function start() {
+        let response = [];
+
+        MEDICINE.forEach((e) => {
+            console.log(e);
+            response.push(predict(e)); //late dumadating ang result ng predict(e)
+        });
+
+        return res.status(200).send(response);
+    }
+    
+    start();
+}
+
 exports.send_prediction_diseases = (req, res, next) => {
 
     const data = {
